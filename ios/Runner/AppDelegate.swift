b89d3e1ce42903b9807c93fdd813c4372f12e67b
Nontaphat Pongpis <momo5101@hotmail.com>
2023-07-18 12:27:08 +0700

    add ios generated plugin

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,30 +10,30 @@ import Flutter
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
             
-            //AR
-            // let controllerAR : FlutterViewController = window?.rootViewController as! FlutterViewController
-            // self.navigationController = UINavigationController(rootViewController: controllerAR)
-            // self.window.rootViewController = self.navigationController
-            // self.navigationController.setNavigationBarHidden(true, animated: false)
-            // self.window.makeKeyAndVisible()
+        GeneratedPluginRegistrant.register(with: self)
             
+        // AR Setup
+        let controllerAR = window?.rootViewController as! FlutterViewController
+        self.navigationController = UINavigationController(rootViewController: controllerAR)
+        self.window.rootViewController = self.navigationController
+        self.navigationController.setNavigationBarHidden(true, animated: false)
+        self.window.makeKeyAndVisible()
             
-            // let ARChannel = FlutterMethodChannel(name: "app.graffity.ar-viewer/ar", binaryMessenger: controllerAR.binaryMessenger)
-            // ARChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            //     if call.method == "OpenAR" {
-            //         if let args = call.arguments as? Dictionary<String, Any>,
-            //            let data = args["data"] as? String {
-            //             let arVC = ARViewController()
-            //             arVC.data = data
-            //             self?.navigationController.setNavigationBarHidden(false, animated: true)
-            //             self?.navigationController.pushViewController(arVC, animated: true)
-            //             //                self?.navigateToARViewController(data: data)
-            //         }
-            //     }
-            // }
-            
-            GeneratedPluginRegistrant.register(with: self)
-            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-        }
-}
+        // Flutter Platform Channel Setup
+        let ARChannel = FlutterMethodChannel(name: "app.graffity.ar-viewer/ar", binaryMessenger: controllerAR.binaryMessenger)
 
+        ARChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            if call.method == "OpenAR" {
+                if let args = call.arguments as? Dictionary<String, Any>,
+                    let data = args["data"] as? String {
+                    let arVC = ARViewController()
+                    arVC.data = data
+                    self?.navigationController?.setNavigationBarHidden(false, animated: true)
+                    self?.navigationController?.pushViewController(arVC, animated: true)
+                }
+            }
+        }
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
