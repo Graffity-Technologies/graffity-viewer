bb572176fc6715791420740fab0e7d0636424921
bankbiz <thaworn@graffity.tech>
2023-08-03 13:32:49 +0700

    regard arMode & update ios

--- ios/Runner/ARViewController.swift
+++ ios/Runner/ARViewController.swift
@@ -11,26 +11,19 @@ import GraffityARCloudService
 class ARViewController: UIViewController {
     var accessToken: String?
     var arMode: String?
-    let graffityARCloud = GraffityARCloud(accessToken: "YOUR_ACCESS_TOKEN")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Use the "accessToken" property to display the passed data
         if let accessToken = accessToken {
-            
-                let graffityARCloud = GraffityARCloud(accessToken: accessToken)
-                let arCloudUIView = ARCloudUIView(service: self.graffityARCloud)
-                self.addChild(arCloudUIView)
-                self.view.addSubview(arCloudUIView.view)
-            } 
-            
-            //            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-            //            label.text = data
-            //            label.textAlignment = .center
-            //            label.center = view.center
-            //            label.backgroundColor = .orange
-            //            view.backgroundColor = .blue
-            //            view.addSubview(label)
+            var isFrontCamera = false
+            if (arMode == "Face Anchor") {
+                isFrontCamera = true
+            }
+            let graffityARCloud = GraffityARCloud(accessToken: accessToken)
+            let arCloudUIView = ARCloudUIView(service: graffityARCloud, frontCameraMode: isFrontCamera)
+            self.addChild(arCloudUIView)
+            self.view.addSubview(arCloudUIView.view)
         }
     }
+}
