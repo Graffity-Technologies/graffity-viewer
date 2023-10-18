70944450ce330ae549a52c0367d51b8ccbeaa4ff
bankbiz <thaworn@graffity.tech>
2023-10-18 17:55:42 +0700

    update web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import AppStoreImage from './images/App Store.png';
+import GooglePlayImage from './images/Google Play.png';
+import ViewerLogo from './images/graffity_viewer_logo.png';
+
 function App() {
+  const appStoreURL = 'https://apps.apple.com/th/app/graffity-viewer/id6451207164';
+  const googlePlayURL = 'https://play.google.com/store/apps/details?id=com.graffity.ar_viewer';
+
   return (
     <div className="App">
-
+      <div className='app-container'>
+        <img
+          src={ViewerLogo}
+          alt="Viewer Logo"
+          width={300}
+        />
+        <br /><br /><br /><br />
+        <p>App is not installed on your device? <br />Download it from App Store or Google Play.</p>
+        <br />
+        <div className='btn-container'>
+          <a className='btn-app-store' href={appStoreURL} target="_blank" rel="noopener noreferrer">
+            <img
+              src={AppStoreImage}
+              alt="Download on the App Store"
+              width={200}
+            />
+          </a>
+          <a href={googlePlayURL} target="_blank" rel="noopener noreferrer">
+            <img
+              src={GooglePlayImage}
+              alt="Get it on Google Play"
+              width={200}
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
