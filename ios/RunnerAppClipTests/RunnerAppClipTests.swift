464eb30747e0a310e71a4a201bbc5d54079d1768
Woraprat Saelo <can_saelo@hotmail.com>
2025-03-18 16:08:08 +0700

    app clip build

---
+++ ios/RunnerAppClipTests/RunnerAppClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerAppClipTests.swift
+//  RunnerAppClipTests
+//
+//  Created by Woraprat Saelo on 13/3/2568 BE.
+//
+
+import Testing
+@testable import RunnerAppClip
+
+struct RunnerAppClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
