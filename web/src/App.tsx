e737e6ab5d7b2be901f610c4378ce8314f94643c
bankbiz <thaworn@graffity.tech>
2025-05-14 02:17:17 +0700

    update download icon on each platform

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import AppStoreImage from './images/App Store.png';
@@ -9,6 +9,34 @@ import { NonAppLauncher } from './components/NonAppLauncher';
 function App() {
   const appStoreURL = 'https://apps.apple.com/th/app/graffity-viewer/id6451207164';
   const googlePlayURL = 'https://play.google.com/store/apps/details?id=com.graffity.ar_viewer';
+  const [platform, setPlatform] = useState('unknown');
+
+  useEffect(() => {
+    // Detect the user's platform
+    const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+      setPlatform('ios');
+    } else if (/android/i.test(userAgent)) {
+      setPlatform('android');
+    } else if (/Mac/.test(userAgent)) {
+      setPlatform('mac'); // macOS devices
+    } else if (/Win/.test(userAgent)) {
+      setPlatform('windows'); // Windows devices
+    } else {
+      setPlatform('unknown');
+    }
+  }, []);
+
+  const renderDownloadText = () => {
+    if (platform === 'ios' || platform === 'mac') {
+      return "Use our App for the best experience. Download it from the App Store.";
+    } else if (platform === 'android') {
+      return "Use our App for the best experience. Download it from Google Play.";
+    } else {
+      return "Use our App for the best experience. Download it from App Store or Google Play.";
+    }
+  };
 
   return (
     <div className="App">
@@ -19,23 +47,27 @@ function App() {
           width={230}
         />
         <br /><br /><br /><br />
-        <p>Use our App for the best experience. <br />Download it from App Store or Google Play.</p>
+        <p>{renderDownloadText()}</p>
         <br />
         <div className='btn-container'>
-          <a className='btn-app-store' href={appStoreURL} target="_blank" rel="noopener noreferrer">
-            <img
-              src={AppStoreImage}
-              alt="Download on the App Store"
-              width={150}
-            />
-          </a>
-          <a href={googlePlayURL} target="_blank" rel="noopener noreferrer">
-            <img
-              src={GooglePlayImage}
-              alt="Get it on Google Play"
-              width={150}
-            />
-          </a>
+          {(platform === 'ios' || platform === 'mac' || platform === 'unknown') && (
+            <a className='btn-app-store' href={appStoreURL} target="_blank" rel="noopener noreferrer">
+              <img
+                src={AppStoreImage}
+                alt="Download on the App Store"
+                width={150}
+              />
+            </a>
+          )}
+          {(platform === 'android' || platform === 'windows' || platform === 'unknown') && (
+            <a href={googlePlayURL} target="_blank" rel="noopener noreferrer">
+              <img
+                src={GooglePlayImage}
+                alt="Get it on Google Play"
+                width={150}
+              />
+            </a>
+          )}
         </div>
         <NonAppLauncher />
       </div>
